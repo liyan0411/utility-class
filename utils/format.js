d4ab4c4b93ac0d098125d431f2352f1286b44a99
liyan <liyan@anji-plus.com>
2020-04-24 14:21:35 +0800

    添加工具类

---
+++ utils/format.js
@@ -0,0 +1,323 @@
+/**
+ * @name 数据格式化
+ */
+
+/**
+ * @name 字符串大小写转化处理
+ * @param  str:String
+ * @param  type:Number(可选)
+ *       type:  1:首字母大写  2：首页母小写  3：大小写转换  4：全部大写(默认)  5：全部小写
+ * @return String
+ */
+export const changeCase = (str, type = 4) => {
+  type = type || 4
+  switch (type) {
+    case 1:
+      return str.replace(/\b\w+\b/g, function (word) {
+        return (
+          word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase()
+        )
+      })
+    case 2:
+      return str.replace(/\b\w+\b/g, function (word) {
+        return (
+          word.substring(0, 1).toLowerCase() + word.substring(1).toUpperCase()
+        )
+      })
+    case 3:
+      return str
+        .split('')
+        .map(function (word) {
+          if (/[a-z]/.test(word)) {
+            return word.toUpperCase()
+          } else {
+            return word.toLowerCase()
+          }
+        })
+        .join('')
+    case 4:
+      return str.toUpperCase()
+    case 5:
+      return str.toLowerCase()
+    default:
+      return str
+  }
+}
+/**
+ * @name 时间戳转化为年月日
+ * @param times:String-时间戳
+ * @param ymd:String可选-格式类型(yyyy-mm-dd,yyyy/mm/dd)
+ * @param hms:String可选-格式类型(hh,hh:mm,hh:mm:ss)
+ * @returns {年月日}
+ */
+export const timesToYyMmDd = (times, ymd = 'yyyy-mm-dd', hms = 'hh:mm:ss') => {
+  const oDate = new Date(times)
+  const oYear = oDate.getFullYear()
+  const oMonth = oDate.getMonth() + 1
+  const oDay = oDate.getDate()
+  const oHour = oDate.getHours()
+  const oMin = oDate.getMinutes()
+  const oSec = oDate.getSeconds()
+  let oTime // 最后拼接时间
+  // 年月日格式
+  switch (ymd) {
+    case 'yyyy-mm-dd':
+      oTime = oYear + '-' + getzf(oMonth) + '-' + getzf(oDay)
+      break
+    case 'yyyy/mm/dd':
+      oTime = oYear + '/' + getzf(oMonth) + '/' + getzf(oDay)
+      break
+  }
+  // 时分秒格式
+  switch (hms) {
+    case 'hh':
+      oTime = ' ' + oTime + getzf(oHour)
+      break
+    case 'hh:mm':
+      oTime = oTime + getzf(oHour) + ':' + getzf(oMin)
+      break
+    case 'hh:mm:ss':
+      oTime = oTime + getzf(oHour) + ':' + getzf(oMin) + ':' + getzf(oSec)
+      break
+  }
+  return oTime
+}
+/**
+ * @name HEX十六进制颜色值转换为RGBA颜色值-如#FFFFFF转RGB(255,255,255)
+ * @param val:String-十六进制颜色值
+ * @param aplha:Number-透明度（默认为1）
+ * @return  RGBA颜色值
+ */
+
+export const hexToRgba = function (val, aplha = 1) {
+  // 16进制颜色值的正则
+  var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/
+  // 把颜色值变成小写
+  var color = val.toLowerCase()
+  if (reg.test(color)) {
+    // 如果只有三位的值，需变成六位，如：#fff => #ffffff
+    if (color.length === 4) {
+      var colorNew = '#'
+      for (var i = 1; i < 4; i += 1) {
+        colorNew += color.slice(i, i + 1).concat(color.slice(i, i + 1))
+      }
+      color = colorNew
+    }
+    // 处理六位的颜色值，转为RGB
+    var colorChange = []
+    for (var i = 1; i < 7; i += 2) {
+      colorChange.push(parseInt('0x' + color.slice(i, i + 2)))
+    }
+    return 'rgba(' + colorChange.join(',') + ',' + aplha + ')'
+  } else {
+    return color
+  }
+}
+
+/**
+ * @name 将阿拉伯数字翻译成中文的大写数字
+ * @param num:Number
+ * @return String
+ */
+export const numberToChinese = (num) => {
+  var AA = new Array(
+    '零',
+    '一',
+    '二',
+    '三',
+    '四',
+    '五',
+    '六',
+    '七',
+    '八',
+    '九',
+    '十'
+  )
+  var BB = new Array('', '十', '百', '仟', '萬', '億', '点', '')
+  var a = ('' + num).replace(/(^0*)/g, '').split('.'),
+    k = 0,
+    re = ''
+  for (var i = a[0].length - 1; i >= 0; i--) {
+    switch (k) {
+      case 0:
+        re = BB[7] + re
+        break
+      case 4:
+        if (!new RegExp('0{4}//d{' + (a[0].length - i - 1) + '}$').test(a[0]))
+          re = BB[4] + re
+        break
+      case 8:
+        re = BB[5] + re
+        BB[7] = BB[5]
+        k = 0
+        break
+    }
+    if (k % 4 == 2 && a[0].charAt(i + 2) != 0 && a[0].charAt(i + 1) == 0)
+      re = AA[0] + re
+    if (a[0].charAt(i) != 0) re = AA[a[0].charAt(i)] + BB[k % 4] + re
+    k++
+  }
+
+  if (a.length > 1) {
+    // 加上小数部分(如果有小数部分)
+    re += BB[6]
+    for (var i = 0; i < a[1].length; i++) re += AA[a[1].charAt(i)]
+  }
+  if (re == '一十') re = '十'
+  if (re.match(/^一/) && re.length == 3) re = re.replace('一', '')
+  return re
+}
+/**
+ * @name 将数字转换为大写金额
+ * @param Num:Number
+ * @return String
+ */
+export const changeToChinese = (Num) => {
+  //判断如果传递进来的不是字符的话转换为字符
+  if (typeof Num == 'number') {
+    Num = new String(Num)
+  }
+  Num = Num.replace(/,/g, '') //替换tomoney()中的“,”
+  Num = Num.replace(/ /g, '') //替换tomoney()中的空格
+  Num = Num.replace(/￥/g, '') //替换掉可能出现的￥字符
+  if (isNaN(Num)) {
+    //验证输入的字符是否为数字
+    //alert("请检查小写金额是否正确");
+    return ''
+  }
+  //字符处理完毕后开始转换，采用前后两部分分别转换
+  var part = String(Num).split('.')
+  var newchar = ''
+  //小数点前进行转化
+  for (var i = part[0].length - 1; i >= 0; i--) {
+    if (part[0].length > 10) {
+      return ''
+      //若数量超过拾亿单位，提示
+    }
+    var tmpnewchar = ''
+    var perchar = part[0].charAt(i)
+    switch (perchar) {
+      case '0':
+        tmpnewchar = '零' + tmpnewchar
+        break
+      case '1':
+        tmpnewchar = '壹' + tmpnewchar
+        break
+      case '2':
+        tmpnewchar = '贰' + tmpnewchar
+        break
+      case '3':
+        tmpnewchar = '叁' + tmpnewchar
+        break
+      case '4':
+        tmpnewchar = '肆' + tmpnewchar
+        break
+      case '5':
+        tmpnewchar = '伍' + tmpnewchar
+        break
+      case '6':
+        tmpnewchar = '陆' + tmpnewchar
+        break
+      case '7':
+        tmpnewchar = '柒' + tmpnewchar
+        break
+      case '8':
+        tmpnewchar = '捌' + tmpnewchar
+        break
+      case '9':
+        tmpnewchar = '玖' + tmpnewchar
+        break
+    }
+    switch (part[0].length - i - 1) {
+      case 0:
+        tmpnewchar = tmpnewchar + '元'
+        break
+      case 1:
+        if (perchar != 0) tmpnewchar = tmpnewchar + '拾'
+        break
+      case 2:
+        if (perchar != 0) tmpnewchar = tmpnewchar + '佰'
+        break
+      case 3:
+        if (perchar != 0) tmpnewchar = tmpnewchar + '仟'
+        break
+      case 4:
+        tmpnewchar = tmpnewchar + '万'
+        break
+      case 5:
+        if (perchar != 0) tmpnewchar = tmpnewchar + '拾'
+        break
+      case 6:
+        if (perchar != 0) tmpnewchar = tmpnewchar + '佰'
+        break
+      case 7:
+        if (perchar != 0) tmpnewchar = tmpnewchar + '仟'
+        break
+      case 8:
+        tmpnewchar = tmpnewchar + '亿'
+        break
+      case 9:
+        tmpnewchar = tmpnewchar + '拾'
+        break
+    }
+    var newchar = tmpnewchar + newchar
+  }
+  //小数点之后进行转化
+  if (Num.indexOf('.') != -1) {
+    if (part[1].length > 2) {
+      // alert("小数点之后只能保留两位,系统将自动截断");
+      part[1] = part[1].substr(0, 2)
+    }
+    for (i = 0; i < part[1].length; i++) {
+      tmpnewchar = ''
+      perchar = part[1].charAt(i)
+      switch (perchar) {
+        case '0':
+          tmpnewchar = '零' + tmpnewchar
+          break
+        case '1':
+          tmpnewchar = '壹' + tmpnewchar
+          break
+        case '2':
+          tmpnewchar = '贰' + tmpnewchar
+          break
+        case '3':
+          tmpnewchar = '叁' + tmpnewchar
+          break
+        case '4':
+          tmpnewchar = '肆' + tmpnewchar
+          break
+        case '5':
+          tmpnewchar = '伍' + tmpnewchar
+          break
+        case '6':
+          tmpnewchar = '陆' + tmpnewchar
+          break
+        case '7':
+          tmpnewchar = '柒' + tmpnewchar
+          break
+        case '8':
+          tmpnewchar = '捌' + tmpnewchar
+          break
+        case '9':
+          tmpnewchar = '玖' + tmpnewchar
+          break
+      }
+      if (i == 0) tmpnewchar = tmpnewchar + '角'
+      if (i == 1) tmpnewchar = tmpnewchar + '分'
+      newchar = newchar + tmpnewchar
+    }
+  }
+  //替换所有无用汉字
+  while (newchar.search('零零') != -1) newchar = newchar.replace('零零', '零')
+  newchar = newchar.replace('零亿', '亿')
+  newchar = newchar.replace('亿万', '亿')
+  newchar = newchar.replace('零万', '万')
+  newchar = newchar.replace('零元', '元')
+  newchar = newchar.replace('零角', '')
+  newchar = newchar.replace('零分', '')
+  if (newchar.charAt(newchar.length - 1) == '元') {
+    newchar = newchar + '整'
+  }
+  return newchar
+}
--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,11 @@
 /**
- * @name:常用工具库
+ * @name 常用工具库
  */
-// 本地缓存 增删改查
+/**
+ * @name 本地缓存 增删改查
+ * @param k:String-键
+ * @param v:String||Object-值
+ */
 const ls = localStorage
 const ss = sessionStorage
 export const db = {
@@ -13,14 +17,12 @@ export const db = {
       } catch (e) {}
     },
     set(k, v) {
-      ls.setItem(key, JSON.stringify(value))
-
       try {
         ls.setItem(k, JSON.stringify(v))
       } catch (e) {}
     },
-    remove(key) {
-      ls.removeItem(key)
+    remove(k) {
+      ls.removeItem(k)
     },
     clear() {
       ls.clear()
@@ -34,14 +36,12 @@ export const db = {
       } catch (e) {}
     },
     set(k, v) {
-      ss.setItem(key, JSON.stringify(value))
-
       try {
         ss.setItem(k, JSON.stringify(v))
       } catch (e) {}
     },
-    remove(key) {
-      ss.removeItem(key)
+    remove(k) {
+      ss.removeItem(k)
     },
     clear() {
       ss.clear()
@@ -49,255 +49,60 @@ export const db = {
   },
 }
 
-export const checkStr = (str, type) => {
-  switch (type) {
-    case 'phone': //手机号码
-      return /^1[3|4|5|6|7|8][0-9]{9}$/.test(str)
-    case 'tel': //座机
-      return /^(0\d{2,3}-\d{7,8})(-\d{1,4})?$/.test(str)
-    case 'card': //身份证
-      return /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/.test(str)
-    case 'pwd': //密码以字母开头，长度在6~18之间，只能包含字母、数字和下划线
-      return /^[a-zA-Z]\w{5,17}$/.test(str)
-    case 'postal': //邮政编码
-      return /[1-9]\d{5}(?!\d)/.test(str)
-    case 'QQ': //QQ号
-      return /^[1-9][0-9]{4,9}$/.test(str)
-    case 'email': //邮箱
-      return /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/.test(str)
-    case 'money': //金额(小数点2位)
-      return /^\d*(?:\.\d{0,2})?$/.test(str)
-    case 'URL': //网址
-      return /(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+#])?/.test(
-        str
-      )
-    case 'IP': //IP
-      return /((?:(?:25[0-5]|2[0-4]\\d|[01]?\\d?\\d)\\.){3}(?:25[0-5]|2[0-4]\\d|[01]?\\d?\\d))/.test(
-        str
-      )
-    case 'date': //日期时间
-      return (
-        /^(\d{4})\-(\d{2})\-(\d{2}) (\d{2})(?:\:\d{2}|:(\d{2}):(\d{2}))$/.test(
-          str
-        ) || /^(\d{4})\-(\d{2})\-(\d{2})$/.test(str)
-      )
-    case 'number': //数字
-      return /^[0-9]$/.test(str)
-    case 'english': //英文
-      return /^[a-zA-Z]+$/.test(str)
-    case 'chinese': //中文
-      return /^[\u4E00-\u9FA5]+$/.test(str)
-    case 'lower': //小写
-      return /^[a-z]+$/.test(str)
-    case 'upper': //大写
-      return /^[A-Z]+$/.test(str)
-    case 'HTML': //HTML标记
-      return /<("[^"]*"|'[^']*'|[^'">])*>/.test(str)
-    default:
-      return true
-  }
-}
-
-// 严格的身份证校验
-export const isCardID = (sId) => {
-  if (!/(^\d{15}$)|(^\d{17}(\d|X|x)$)/.test(sId)) {
-    alert('你输入的身份证长度或格式错误')
-    return false
-  }
-  //身份证城市
-  var aCity = {
-    11: '北京',
-    12: '天津',
-    13: '河北',
-    14: '山西',
-    15: '内蒙古',
-    21: '辽宁',
-    22: '吉林',
-    23: '黑龙江',
-    31: '上海',
-    32: '江苏',
-    33: '浙江',
-    34: '安徽',
-    35: '福建',
-    36: '江西',
-    37: '山东',
-    41: '河南',
-    42: '湖北',
-    43: '湖南',
-    44: '广东',
-    45: '广西',
-    46: '海南',
-    50: '重庆',
-    51: '四川',
-    52: '贵州',
-    53: '云南',
-    54: '西藏',
-    61: '陕西',
-    62: '甘肃',
-    63: '青海',
-    64: '宁夏',
-    65: '新疆',
-    71: '台湾',
-    81: '香港',
-    82: '澳门',
-    91: '国外',
-  }
-  if (!aCity[parseInt(sId.substr(0, 2))]) {
-    alert('你的身份证地区非法')
-    return false
-  }
-
-  // 出生日期验证
-  var sBirthday = (
-      sId.substr(6, 4) +
-      '-' +
-      Number(sId.substr(10, 2)) +
-      '-' +
-      Number(sId.substr(12, 2))
-    ).replace(/-/g, '/'),
-    d = new Date(sBirthday)
-  if (
-    sBirthday !=
-    d.getFullYear() + '/' + (d.getMonth() + 1) + '/' + d.getDate()
-  ) {
-    alert('身份证上的出生日期非法')
-    return false
-  }
-
-  // 身份证号码校验
-  var sum = 0,
-    weights = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2],
-    codes = '10X98765432'
-  for (var i = 0; i < sId.length - 1; i++) {
-    sum += sId[i] * weights[i]
-  }
-  var last = codes[sum % 11] //计算出来的最后一位身份证号码
-  if (sId[sId.length - 1] != last) {
-    alert('你输入的身份证号非法')
-    return false
-  }
-
-  return true
-}
-
-//HEX十六进制颜色值转换为RGB(A)颜色值-如#FFFFFF转RGB(255,255,255)
-export const hexToRgba = function (val, aplha = 1) {
-  // 16进制颜色值的正则
-  var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/
-  // 把颜色值变成小写
-  var color = val.toLowerCase()
-  if (reg.test(color)) {
-    // 如果只有三位的值，需变成六位，如：#fff => #ffffff
-    if (color.length === 4) {
-      var colorNew = '#'
-      for (var i = 1; i < 4; i += 1) {
-        colorNew += color.slice(i, i + 1).concat(color.slice(i, i + 1))
-      }
-      color = colorNew
-    }
-    // 处理六位的颜色值，转为RGB
-    var colorChange = []
-    for (var i = 1; i < 7; i += 2) {
-      colorChange.push(parseInt('0x' + color.slice(i, i + 2)))
-    }
-    return 'rgba(' + colorChange.join(',') + ',' + aplha + ')'
-  } else {
-    return color
-  }
-}
-
 /**
- * @param {String} param 根据传入值判断是否为空（无效）
- * @return {String} 判断值状态 空（无效）返回true
+ * @name 获取 url 后面通过?传参的参数
+ * @param name:String
+ * @return String
  */
-export const isEmpty = (text) => {
-  if (typeof text === 'object') {
-    // 判断是否为空对象
-    for (const x in text) {
-      return false
-    }
-
-    return true
-  }
-  if (
-    text == null ||
-    text == undefined ||
-    text == 'undefined' ||
-    text == '' ||
-    text == 'null'
-  ) {
-    return true
-  }
-
-  return false
-}
-/**
- *  获取 url 后面通过?传参的参数
- * @param {String} name
- */
-export function getQueryString(name) {
-  const reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i')
-  const url = window.location.href
-  const search = url.substring(url.lastIndexOf('?') + 1)
-  const r = search.match(reg)
+export const getUrlParams = (name) => {
+  var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i')
+  var r = window.location.search.substr(1).match(reg)
   if (r != null) return unescape(r[2])
   return null
 }
 /**
- * 防抖函数
- * @param method 事件触发的操作
- * @param delay 多少毫秒内连续触发事件，不会执行
- * @returns {Function}
+ * @name 通用版节流函数
+ * @param fn:function-要被执行的方法, 相隔多长时间要被执行的方法
+ * @param Intervals:number  - 间隔时间, 相隔多长时间调用一次对应方法
+ * @params args:any  - 剩余参数,剩余参数将会在调用fn时作为参数传给fn
+ * @params params:any  - 以下方使用例子看，resize事件被触发的时候，会在传个event对象过去，所以同样需要接收
+ * @return Function
  */
-export const debounce = (method, delay) => {
-  let timer = null
-  return function () {
-    let self = this,
-      args = arguments
-    timer && clearTimeout(timer)
-    timer = setTimeout(function () {
-      method.apply(self, args)
-    }, delay)
+export const throttle = (fn, Intervals, ...args) => {
+  let timeNo
+  return (...params) => {
+    if (timeNo) return
+    timeNo = setTimeout(() => {
+      fn(...args, ...params)
+      clearTimeout(timeNo)
+      timeNo = null
+    }, Intervals)
   }
 }
-// window.onscroll = debounce(function () {
-//   let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-//   console.log("滚动条位置：" + scrollTop);
-// }, 200);
 /**
- * 节流函数
- * @param method 事件触发的操作
- * @param mustRunDelay 间隔多少毫秒需要触发一次事件
+ * @name 通用版防抖函数
+ * @param fn:function  - 要被执行的方法, 相隔多长时间要被执行的方法
+ * @param Intervals:number  - 间隔时间, 相隔多长时间调用一次对应方法
+ * @params args:any  - 剩余参数,剩余参数将会在调用fn时作为参数传给fn
+ * @params params:any  - 以下方使用例子看，input事件被触发的时候，会在传个event对象过去，所以同样需要接收
+ * @return Function
  */
-export const throttle = (method, mustRunDelay) => {
-  let timer,
-    args = arguments,
-    start
-  return function loop() {
-    let self = this
-    let now = Date.now()
-    if (!start) {
-      start = now
-    }
-    if (timer) {
-      clearTimeout(timer)
-    }
-    if (now - start >= mustRunDelay) {
-      method.apply(self, args)
-      start = now
-    } else {
-      timer = setTimeout(function () {
-        loop.apply(self, args)
-      }, 50)
-    }
+export const debounce = (fn, Intervals, ...args) => {
+  let timeNo
+  return (...params) => {
+    clearTimeout(timeNo)
+    timeNo = setTimeout(() => {
+      fn(...args, ...params)
+      clearTimeout(timeNo)
+    }, Intervals)
   }
 }
-// window.onscroll = throttle(function () {
-//   let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-//   console.log("滚动条位置：" + scrollTop);
-// }, 800);
 
-// 导出文件函数封装
+/**
+ * @name 导出文件函数封装
+ * @param data:Blob-后台返回的流，names:String-导出的文件名
+ * @return Void
+ */
 export const downloadFile = function (data, names) {
   if (!data) {
     return
@@ -320,183 +125,207 @@ export const downloadFile = function (data, names) {
     link.click()
   }
 }
+
 /**
- * 将字母全部转化成小写
- * @param {String} str 字符
- */
-export const strToLowercase = (str) => {
-  return str.toLowerCase()
-}
-/**
- * 将字母全部转化成大写
- * @param {String} str 字符
- */
-export const strToCapital = (str) => {
-  return str.toUpperCase()
-}
-/**
- * 时间戳转化为年月日
- * @param times 时间戳
- * @param ymd 格式类型(yyyy-mm-dd,yyyy/mm/dd)
- * @param hms 可选,格式类型(hh,hh:mm,hh:mm:ss)
- * @returns {年月日}
- */
-export const timesToYyMmDd = (times, ymd = 'yyyy-mm-dd', hms = 'hh:mm:ss') => {
-  const oDate = new Date(times)
-  const oYear = oDate.getFullYear()
-  const oMonth = oDate.getMonth() + 1
-  const oDay = oDate.getDate()
-  const oHour = oDate.getHours()
-  const oMin = oDate.getMinutes()
-  const oSec = oDate.getSeconds()
-  let oTime // 最后拼接时间
-  // 年月日格式
-  switch (ymd) {
-    case 'yyyy-mm-dd':
-      oTime = oYear + '-' + getzf(oMonth) + '-' + getzf(oDay)
-      break
-    case 'yyyy/mm/dd':
-      oTime = oYear + '/' + getzf(oMonth) + '/' + getzf(oDay)
-      break
-  }
-  // 时分秒格式
-  switch (hms) {
-    case 'hh':
-      oTime = ' ' + oTime + getzf(oHour)
-      break
-    case 'hh:mm':
-      oTime = oTime + getzf(oHour) + ':' + getzf(oMin)
-      break
-    case 'hh:mm:ss':
-      oTime = oTime + getzf(oHour) + ':' + getzf(oMin) + ':' + getzf(oSec)
-      break
-  }
-  return oTime
-}
-/**
- * 返回指定长度的月份集合
- * 
- * @param  {time} 时间
- * @param  {len} 长度
- * @param  {direction} 方向：  1: 前几个月;  2: 后几个月;  3:前后几个月  默认 3
- * @return {Array} 数组
- * 
+ * @name 返回指定长度的月份集合
+ * @param  time:String-时间
+ * @param  len:Number-长度
+ * @param  direction:Numver-方向(1: 前几个月;  2: 后几个月;  3:前后几个月  默认 3)
+ * @return Array 数组
+ *
  * @example   getMonths('2018-1-29', 6, 1)  // ->  ["2018-1", "2017-12", "2017-11", "2017-10", "2017-9", "2017-8", "2017-7"]
  */
-export const getMonths=(time, len, direction) =>{
-    var mm = new Date(time).getMonth(),
-        yy = new Date(time).getFullYear(),
-        direction = isNaN(direction) ? 3 : direction,
-        index = mm;
-    var cutMonth = function(index) {
-        if ( index <= len && index >= -len) {
-            return direction === 1 ? formatPre(index).concat(cutMonth(++index)):
-                direction === 2 ? formatNext(index).concat(cutMonth(++index)):formatCurr(index).concat(cutMonth(++index))
+export const getMonths = (time, len, direction) => {
+  var mm = new Date(time).getMonth(),
+    yy = new Date(time).getFullYear(),
+    direction = isNaN(direction) ? 3 : direction,
+    index = mm
+  var cutMonth = function (index) {
+    if (index <= len && index >= -len) {
+      return direction === 1
+        ? formatPre(index).concat(cutMonth(++index))
+        : direction === 2
+        ? formatNext(index).concat(cutMonth(++index))
+        : formatCurr(index).concat(cutMonth(++index))
+    }
+    return []
+  }
+  var formatNext = function (i) {
+    var y = Math.floor(i / 12),
+      m = i % 12
+    return [yy + y + '-' + (m + 1)]
+  }
+  var formatPre = function (i) {
+    var y = Math.ceil(i / 12),
+      m = i % 12
+    m = m === 0 ? 12 : m
+    return [yy - y + '-' + (13 - m)]
+  }
+  var formatCurr = function (i) {
+    var y = Math.floor(i / 12),
+      yNext = Math.ceil(i / 12),
+      m = i % 12,
+      mNext = m === 0 ? 12 : m
+    return [yy - yNext + '-' + (13 - mNext), yy + y + '-' + (m + 1)]
+  }
+  // 数组去重
+  var unique = function (arr) {
+    if (Array.hasOwnProperty('from')) {
+      return Array.from(new Set(arr))
+    } else {
+      var n = {},
+        r = []
+      for (var i = 0; i < arr.length; i++) {
+        if (!n[arr[i]]) {
+          n[arr[i]] = true
+          r.push(arr[i])
         }
-        return []
+      }
+      return r
     }
-    var formatNext = function(i) {
-        var y = Math.floor(i/12),
-            m = i%12
-        return [yy+y + '-' + (m+1)]
-    }
-    var formatPre = function(i) {
-        var y = Math.ceil(i/12),
-            m = i%12
-        m = m===0 ? 12 : m
-        return [yy-y + '-' + (13 - m)]
-    }
-    var formatCurr = function(i) {
-        var y = Math.floor(i/12),
-            yNext = Math.ceil(i/12),
-            m = i%12,
-            mNext = m===0 ? 12 : m
-        return [yy-yNext + '-' + (13 - mNext),yy+y + '-' + (m+1)]
-    }
-    // 数组去重
-    var unique = function(arr) {
-        if ( Array.hasOwnProperty('from') ) {
-            return Array.from(new Set(arr));
-        }else{
-            var n = {},r=[]; 
-            for(var i = 0; i < arr.length; i++){
-                if (!n[arr[i]]){
-                    n[arr[i]] = true; 
-                    r.push(arr[i]);
-                }
-            }
-            return r;
-        }
-    }
-    return direction !== 3 ? cutMonth(index) : unique(cutMonth(index).sort(function(t1, t2){
-        return new Date(t1).getTime() - new Date(t2).getTime()
-    }))
+  }
+  return direction !== 3
+    ? cutMonth(index)
+    : unique(
+        cutMonth(index).sort(function (t1, t2) {
+          return new Date(t1).getTime() - new Date(t2).getTime()
+        })
+      )
 }
 /**
- * 返回指定长度的天数集合
- * 
- * @param  {time} 时间
- * @param  {len} 长度
- * @param  {direction} 方向： 1: 前几天;  2: 后几天;  3:前后几天  默认 3
+ * @name 返回指定长度的天数集合
+ * @param  time:String-时间
+ * @param  len:Number-长度
+ * @param  direction:Numver-方向(1: 前几天;  2: 后几天;  3:前后几天  默认 3)
  * @return {Array} 数组
  *
  * @example date.getDays('2018-1-29', 6) // -> ["2018-1-26", "2018-1-27", "2018-1-28", "2018-1-29", "2018-1-30", "2018-1-31", "2018-2-1"]
  */
-export const getDays=(time, len, diretion)=> {
-    var tt = new Date(time)
-    var getDay = function(day) {
-        var t = new Date(time)
-        t.setDate(t.getDate() + day)
-        var m = t.getMonth()+1
-        return t.getFullYear()+'-'+m+'-'+t.getDate()
+export const getDays = (time, len, diretion) => {
+  var tt = new Date(time)
+  var getDay = function (day) {
+    var t = new Date(time)
+    t.setDate(t.getDate() + day)
+    var m = t.getMonth() + 1
+    return t.getFullYear() + '-' + m + '-' + t.getDate()
+  }
+  var arr = []
+  if (diretion === 1) {
+    for (var i = 1; i <= len; i++) {
+      arr.unshift(getDay(-i))
     }
-    var arr = []
-    if (diretion === 1) {
-        for (var i = 1; i <= len; i++) {
-            arr.unshift(getDay(-i))
-        }
-    }else if(diretion === 2) {
-        for (var i = 1; i <= len; i++) {
-            arr.push(getDay(i))
-        }
-    }else {
-        for (var i = 1; i <= len; i++) {
-            arr.unshift(getDay(-i))
-        }
-        arr.push(tt.getFullYear()+'-'+(tt.getMonth()+1)+'-'+tt.getDate())
-        for (var i = 1; i <= len; i++) {
-            arr.push(getDay(i))
-        }
+  } else if (diretion === 2) {
+    for (var i = 1; i <= len; i++) {
+      arr.push(getDay(i))
     }
-    return diretion === 1 ? arr.concat([tt.getFullYear()+'-'+(tt.getMonth()+1)+'-'+tt.getDate()]) : 
-        diretion === 2 ? [tt.getFullYear()+'-'+(tt.getMonth()+1)+'-'+tt.getDate()].concat(arr) : arr
+  } else {
+    for (var i = 1; i <= len; i++) {
+      arr.unshift(getDay(-i))
+    }
+    arr.push(tt.getFullYear() + '-' + (tt.getMonth() + 1) + '-' + tt.getDate())
+    for (var i = 1; i <= len; i++) {
+      arr.push(getDay(i))
+    }
+  }
+  return diretion === 1
+    ? arr.concat([
+        tt.getFullYear() + '-' + (tt.getMonth() + 1) + '-' + tt.getDate(),
+      ])
+    : diretion === 2
+    ? [
+        tt.getFullYear() + '-' + (tt.getMonth() + 1) + '-' + tt.getDate(),
+      ].concat(arr)
+    : arr
 }
 /**
- * 去除空格
- * @param  {str}
- * @param  {type} 
- *       type:  1-所有空格  2-前后空格  3-前空格 4-后空格
- * @return {String}
+ * @name 去除空格
+ * @param  str:String
+ * @param  type: Numver- 1-所有空格  2-前后空格  3-前空格 4-后空格
+ * @return String
  */
-export const trim = (str, type) =>{
-    type = type || 1
-    switch (type) {
-        case 1:
-            return str.replace(/\s+/g, "");
-        case 2:
-            return str.replace(/(^\s*)|(\s*$)/g, "");
-        case 3:
-            return str.replace(/(^\s*)/g, "");
-        case 4:
-            return str.replace(/(\s*$)/g, "");
-        default:
-            return str;
-    }
+export const trim = (str, type = 1) => {
+  switch (type) {
+    case 1:
+      return str.replace(/\s+/g, '')
+    case 2:
+      return str.replace(/(^\s*)|(\s*$)/g, '')
+    case 3:
+      return str.replace(/(^\s*)/g, '')
+    case 4:
+      return str.replace(/(\s*$)/g, '')
+    default:
+      return str
+  }
 }
 
 /*获取网址参数*/
-export const getURL=(name)=>{
-    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
-    var r = decodeURI(window.location.search).substr(1).match(reg);
-    if(r!=null) return  r[2]; return null;
+export const getURL = (name) => {
+  var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)')
+  var r = decodeURI(window.location.search).substr(1).match(reg)
+  if (r != null) return r[2]
+  return null
+}
+/**
+ * @name 对象转URL
+ * @param data:Object
+ * @return String
+ */
+export const urlEncode = (data) => {
+  var _result = []
+  for (var key in data) {
+    var value = data[key]
+    if (value.constructor == Array) {
+      value.forEach((_value) => {
+        _result.push(key + '=' + _value)
+      })
+    } else {
+      _result.push(key + '=' + value)
+    }
+  }
+  return _result.join('&')
+}
+
+/**
+ * @name 遍历对象
+ * @param obj:Object
+ * @param callback:Function
+ * @return Void
+ */
+export const objForEach = (obj, callback) => {
+  Object.keys(obj).forEach((key) => {
+    callback(obj[key], key)
+  })
+}
+/**
+ * @name 判断一个元素是否在数组中
+ * @param arr:Array
+ * @param val:要判断的值
+ * @return Boolean
+ */
+export const contains = (arr, val) => {
+  return arr.indexOf(val) != -1 ? true : false
+}
+/**
+ * @name 求和
+ * @param arr:Array
+ * @return Number
+ */
+export const sum = (arr) => {
+  return arr.reduce((pre, cur) => {
+    return pre + cur
+  })
+}
+/** 
+ * @name 随机数范围
+ * @param min:Number-最小值
+ * @param max:Number-最大值
+ * @return Number
+ */
+export const random =(min, max) =>{
+    if (arguments.length === 2) {
+        return Math.floor(min + Math.random() * ( (max+1) - min ))
+    }else{
+        return null;
+    }
+    
 }
